831be7e6b410ad591e16be721cd8d1567562a6e3
Geremia Nappo <nappo.geremia@gmail.com>
2025-01-13 10:43:33 +0100

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,52 @@
+import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+import dts from 'rollup-plugin-dts';
+import del from 'rollup-plugin-delete';
+import copy from 'rollup-plugin-copy';
+import packageJson from './package.json' assert { type: 'json' };
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+        inlineDynamicImports: true
+      },
+      {
+        file: packageJson.module,
+        format: 'es',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      del({ targets: 'dist/*' }),
+      resolve(),
+      commonjs(),
+      typescript({
+        tsconfig: './tsconfig.json',
+        declaration: true,
+        declarationDir: './dist',
+        sourceMap: true,
+      }),
+      terser(),
+      copy({
+        targets: [
+          { src: 'lib/pdf-merger/target/cli.jar', dest: 'dist' }
+        ]
+      }),
+    ],
+    external: [
+      'node:fs', 'node:path'
+    ],
+  },
+  {
+    input: 'dist/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
+    plugins: [dts()],
+  },
+];
